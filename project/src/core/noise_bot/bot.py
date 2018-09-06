d7726ed5024dedb3f1fd8ae23798e869bae27b4e
Bernardo Fontes <bernardoxhc@gmail.com>
2018-09-06 16:40:22 -0300

    Implementa bot para utulizar o modelo treinado da AI

--- project/src/core/noise_bot/bot.py
+++ project/src/core/noise_bot/bot.py
@@ -1,4 +1,8 @@
 import random
+import shlex
+import subprocess
+
+from django.conf import settings
 
 
 class NoiseBot:
@@ -18,3 +22,47 @@ class NoiseBot:
         text_as_list = list(text)
         random.shuffle(text_as_list)
         return ''.join(text_as_list)
+
+
+class BienalBot:
+
+    def run_char_rnn(self, cmd):
+        try:
+            return subprocess.check_output(
+                shlex.split(cmd),
+                cwd=settings.CHAR_RNN_DIR
+            ).strip().decode('utf-8')
+        except subprocess.CalledProcessError:
+            return ''
+
+    def speak_random_line(self):
+        cmd = ' '.join([
+            'th',
+            'sample.lua',
+            settings.CHAR_RNN_MODEL,
+            '-gpuid',
+            '-1',
+            '-verbose',
+            '0',
+            '-length',
+            '200',
+            '-seed',
+            str(random.choice(range(0, 10000000)))
+        ])
+        return self.run_char_rnn(cmd)
+
+    def reply_to(self, text):
+        cmd = ' '.join([
+            'th',
+            'sample.lua',
+            settings.CHAR_RNN_MODEL,
+            '-gpuid',
+            '-1',
+            '-verbose',
+            '0',
+            '-length',
+            '200',
+            '-primetext',
+            '"{}"'.format(text)
+        ])
+        return self.run_char_rnn(cmd)
